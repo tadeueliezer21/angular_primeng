05cbd504b401b8de338e5dbff2edce6bf4896025
Eliezer Tadeu <silva.eliezertadeu@gmail.com>
2019-12-11 20:50:13 -0300

    Criando modulos

---
+++ src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,39 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+import { TableModule } from 'primeng/components/table/table';
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+
+import { SharedModule } from './../shared/shared.module';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaGridComponent } from './pessoa-grid/pessoa-grid.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputMaskModule,
+
+    SharedModule
+  ],
+  declarations: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent,
+    PessoaGridComponent
+  ],
+  exports: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent
+  ]
+})
+export class PessoasModule { }
